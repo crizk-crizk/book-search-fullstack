9568fd1aa45206e2ed39becb13187c20320f5610
Carolina R <carolinarizk@gmail.com>
2021-03-20 16:39:50 -0400

    finished book api routes

--- backend/controllers/bookController.js
+++ backend/controllers/bookController.js
@@ -13,24 +13,46 @@ const getBooks = async (req, res) => {
 // @Desc add a book
 // @Routes POST /api/books
 // @Access public
-const addBook = asyncHandler( async (req, res) => {
+const addBook = async (req, res) => {
   const { title, authors, description, image, link } = req.body;
   console.log(req.body);
   //query to add a book
-  const bookExist = await Book.findOne({ title: req.body.title });
-  if (bookExist) {
-    res.status(400).send("Book already saved");
-  }
-  const book = await Book.create({ title, authors, description, image, link });
-  if (book) {
-    res.status(201).json({
-      message: "Book added",
-      id: book._id,
+  try {
+    const bookExist = await Book.findOne({ title });
+    if (bookExist) {
+      console.log("hi");
+      res.status(400).json({
+        message: "Book already saved",
+        id: bookExist._id,
+        book: req.body,
+      });
+    }
+    const book = await Book.create({
+      title,
+      authors,
+      description,
+      image,
+      link,
     });
-  } else {
-    res.status(400).send("Invalid book data");
+    if (book) {
+      res.status(201).json({
+        message: "Book added",
+        id: book._id,
+      });
+    } else {
+      res.status(400).json({
+        message: "Error",
+        book: req.body,
+      });
+    }
+  } catch (error) {
+    console.error(error)
+    res.status(400).json({
+      message: "Error",
+      book: req.body,
+    });
   }
-});
+};
 
 // @Desc delete a book
 // @Routes DELETE /api/books/:id
@@ -38,7 +60,26 @@ const addBook = asyncHandler( async (req, res) => {
 const deleteBook = async (req, res) => {
   console.log(req.params);
   //query to delete a book
-  res.json("book deleting");
+  try {
+    const deleteThisBook = await Book.deleteOne({ _id: req.params.id });
+    console.log(deleteThisBook);
+    if (deleteThisBook.deletedCount === 1) {
+      res.status(201).json({
+        message: "Book deleted",
+        id: req.params.id,
+      });
+    } else {
+      res.status(404).json({
+        message: "No book found with that ID",
+        id: req.params.id,
+      });
+    }
+  } catch (error) {
+    res.status(400).json({
+      message: "Invalid ID",
+      id: req.params.id,
+    });
+  }
 };
 
 export { getBooks, addBook, deleteBook };
