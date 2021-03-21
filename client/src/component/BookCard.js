6188eea2c52e3f97d84f7bfcaaea1ea492ef727b
Carolina R <carolinarizk@gmail.com>
2021-03-21 16:46:22 -0400

    added the book card

---
+++ client/src/component/BookCard.js
@@ -0,0 +1,38 @@
+import axios from "axios";
+import React from "react";
+
+const BookCard = ({ book, type }) => {
+  const handleBookSave = (event) => {
+    event.preventDefault();
+    axios.post("/api/books", book);
+  };
+
+  const handleBookView = (event) => {
+    event.preventDefault();
+    window.open(book.link);
+  };
+
+  const handleBookDelete = () => {};
+
+  return (
+    <div>
+      <h1>{book.title}</h1>
+      <h3>Written By {book.authors.join(", ")}</h3>
+
+      <img
+        src={book.image}
+        alt="Book cover"
+      />
+
+      <p>{book.description}</p>
+
+      <button onClick={handleBookView}>View</button>
+
+      <button onClick={type === "search" ? handleBookSave : handleBookDelete}>
+        {type === "search" ? "Save" : "Delete"}
+      </button>
+    </div>
+  );
+};
+
+export default BookCard;
