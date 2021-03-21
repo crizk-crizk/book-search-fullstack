d8bc9d4daaa3243fb28f16523662f243f05c6b66
Carolina R <carolinarizk@gmail.com>
2021-03-21 16:07:20 -0400

    added react routes

--- client/src/App.js
+++ client/src/App.js
@@ -3,21 +3,24 @@ import Header from "./component/Header";
 import SavedPage from "./pages/SavedPage";
 import SearchPage from "./pages/SearchPage";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import { StoreProvider } from "./utils/GlobalState";
 
 function App() {
   return (
     <Router>
-      <Header />
-      <div className="App">
-        <Switch>
-          <Route path="/saved">
-            <SavedPage />
-          </Route>
-          <Route path="/">
-            <SearchPage />
-          </Route>
-        </Switch>
-      </div>
+      <StoreProvider>
+        <Header />
+        <div className="App">
+          <Switch>
+            <Route path="/saved">
+              <SavedPage />
+            </Route>
+            <Route path="/">
+              <SearchPage />
+            </Route>
+          </Switch>
+        </div>
+      </StoreProvider>
     </Router>
   );
 }
